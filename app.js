feda95de33894190e618e234e04f62390e3fdc20
Edson <edson_vs09@hotmail.com>
2024-11-24 12:51:38 -0300

    routes teste

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
+
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
+
+const userRoutes = require('./routes/userRoutes');
+
+app.use(express.json());
+app.use('/', userRoutes);
 
 app.get("/" ,(req, res) => {
     res.send("ola mundo")
